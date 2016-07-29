66d141475e315ad2c6ad704eaf43a8e3f8f43401
Zeyugao <gaozeyu@icloud.com>
2016-07-29 20:48:48 +0800

    ..

--- Sources/Noip 2015 推销员.h
+++ Sources/Noip 2015 推销员.h
@@ -23,10 +23,30 @@ T 表示当前划分界限到胡同入口的距离，注意右边部分的贡献
 只需要重新对左边住户的贡献进行排序。于是可以建一个新优先队列将左边的所有住户加入，
 将原优先队列中被划分到左边部分的元素丢掉。因为划分界线是一直往右移的，
 所以每个元素至多被加入两次，被删除两次，总时间复杂度为O(nlogn).*/
+#include <stdio.h>
+#include <queue>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
 #define maxn 1000010
 #define LL long long
 int n, S[maxn], A[maxn];
 bool has[maxn];
+const int BufferSize = 1 << 16;
+char buffer[BufferSize], *Head, *Tail;
+inline char Getchar() {
+    if(Head == Tail) {
+        int l = fread(buffer, 1, BufferSize, stdin);
+        Tail = (Head = buffer) + l;
+    }
+    return *Head++;
+}
+int read() {
+    int x = 0, f = 1; char tc = getchar();
+    while(!isdigit(tc)){ if(tc == '-') f = -1; tc = getchar(); }
+    while(isdigit(tc)){ x = x * 10 + tc - '0'; tc = getchar(); }
+    return x * f;
+}
 struct HeapNode {
 	int id, x;
 	LL val;
@@ -59,7 +79,8 @@ int main() {
 			if (u.x > T && Q.empty()) u = /*(HeapNode)*/{ 0, 0, 0 };
 			//当u.x>T时而Q不空时，ans+=A[i]+2*(S[i]-T)
 			//如果u.x<=T而Q不空时，不会有这种情况
-			//如果u.x<=T且Q空时，此时
+			//如果u.x<=T且Q空时， don't understand
+			//如果u.x>T且Q空，     __|
 			else u.val -= 2ll * T;
 		}
 		HeapNode v; v = /*(HeapNode) */{ 0, 0, 0 };
@@ -69,7 +90,9 @@ int main() {
 		printf("%lld\n", ans);
 		if (u.x > T) {
 			T = u.x;
-			//Tid保存T是第几个数
+			//Tid保存T是第几个数，
+			//将在划分线左边的全部收入
+			//这里的全部收入就只是普通的推销劳累值，没有加上距离
 			for (++Tid; Tid <= n && S[Tid] <= T; Tid++) if (!has[Tid])
 				Q2.push(/*(HeapNode)*/{ Tid, S[Tid], A[Tid] });
 		}
