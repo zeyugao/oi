27ed1df1efafa59fb6c46ff5d1e5082e0c28ad11
Zeyugao <gaozeyu@icloud.com>
2016-08-02 17:44:01 +0800

    noip 2006&1999

---
+++ Sources/Noip 1999 回文数.cpp
@@ -0,0 +1,77 @@
+#ifndef _DEBUG
+#pragma GCC optimize(2)
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <cstring>
+#include <fstream>
+#include <cmath>
+#include <Windows.h>
+#include <iterator>
+#include <set>
+using namespace std;
+#endif // _DEBUG
+#define _m(x) memset(x,0,sizeof(x))
+int ary;
+char m[1001];
+char a[1001];
+int len;
+bool check() {
+	for (int i = 1, j = len; i <= j; i++, j--) {
+		if (m[i] != m[j])return false;
+	}
+	return true;
+}
+void _plus() {
+	char c[1001];
+	_m(c);
+	int i;
+	for (i = 1; i <= len; i++) {
+		c[i] += m[i] + m[len-i+1];
+		c[i+1]+= c[i] / ary;
+		c[i] %= ary;
+	}
+	if (c[i] != 0) {
+		int j = 1;
+		len = i;
+		while (i > 0)
+			m[j++] = c[i--];
+	}
+	else {
+		i -= 1;
+		int j = 1;
+		len = i;
+		while (i > 0)
+			m[j++] = c[i--];
+	}
+}
+int main() {
+	cin >> ary >> a;
+	_m(m);
+	for (int i = 0; i < strlen(a); i++) {
+		if (a[i] < 'A')
+			m[i+1] =a[i]- '0';
+		else
+			m[i+1] =a[i]- 'A' + 10;
+	}
+	len = strlen(a);
+	int times = 0;
+	while (times <= 30) {
+		_plus();
+		if (check()) {
+			printf_s("STEP=%d", times + 1);
+			system("pause");
+			return 0;
+		}
+		times++;
+	}
+	cout << "Impossible!";
+	system("pause");
+	return 0;
+}
---
+++ Sources/Noip 2006 作业调度方案.cpp
@@ -0,0 +1,73 @@
+//My thought:
+//一开始我也认为就是不断地“压入”作业，如果该台机器在有空位（符合条件）就挤入
+//感觉也是挺水的
+//但一开始的题目中“另一方面，在安排后面的操作时，不能改动前面已安排的操作的工作状态。”理解不对，导致否定了自己的想法，认为不能简单地用这种方法来做，exploded!
+//比如说我有一个工件A和一个工件B，A要求机器1先工作2h，再在机器2工作1h，工件B需要在机器2上工作3h，
+//如果没有那句话，我在机器2[2,3]区间插入第2道工序，再看工件B时，我可以把第2道工序往后移1割单位，在机器2的[0,3]插入工件B，最终只需要4h，但如果不允许改前面
+//还是想复杂了
+
+#include<stdio.h>
+#define N 30
+int CPU[N][N]={0};		//定义机器并初始化为未工作状态
+
+typedef struct workprice
+{
+	int count;			//工件计数器，计算工件加工到了第几步
+	int machine[N];		//工件x(1-n)第y(1-m)步使用的机器编号
+	int time[N];		//工件x(1-n)第y(1-m)步使用的时间
+	int endtime;		//工件上次结束时的时间
+}Work;
+
+//判断该段时间是机器否有空，参数为(机器编号， 开始时间， 结束时间)
+int OK(int Mnum, int begin, int end)
+{
+	int i;
+	for(i=begin; i<=end; i++)
+		if(CPU[Mnum][i])
+			return 1;
+	return 0;
+}
+
+int main()
+{
+	Work G[N];									//定义工件
+	int train[361];								//定义队列记录给定顺序
+
+	int m, n, i, j, begin, machnum, step, time, ans=0;
+
+	scanf("%d %d", &m, &n);
+	for(i=1; i<=n; i++)							//初始化结构体变量的计数器和结束时间
+	{	G[i].count=0;	G[i].endtime=1;	}
+
+	for(i=1; i<=m*n; i++)						//输入给定安排顺序
+		scanf("%d", &train[i]);
+
+	for(i=1; i<=n; i++)
+		for(j=1; j<=m; j++)						//输入第i个工件第j步使用的机器编号
+			scanf("%d", &G[i].machine[j]);
+
+	for(i=1; i<=n; i++)	
+		for(j=1; j<=m; j++)						//输入第i个工件第j步需要花费的时间
+			scanf("%d", &G[i].time[j]);
+
+//核心代码
+	for(i=1; i<=m*n; i++)
+	{
+		step = ++G[train[i]].count;				//找到工件加工到了第几步
+		begin = G[train[i]].endtime;			//找到该工件上次的结束时间记录为本次开始时间
+		machnum = G[train[i]].machine[step];	//找到加工该工件第step步需要的机器号
+		time = G[train[i]].time[step];			//找到加工该工件第step步需要的时间
+
+		//找出实际能够开始的时间（即机器有足够长的时间能够完成加工该步骤的任务）
+		while(OK(machnum, begin, begin+time-1))		begin++;
+
+		for(j=begin; j<begin+time; j++)			//将该段时间标记为已经使用
+			CPU[machnum][j]=1;
+
+		ans = ans>(begin+time-1)?ans:(begin+time-1);	//取机器运行时间的最大值作为结果
+
+		G[train[i]].endtime=begin+time;			//刷新该工件的结束时间
+	}
+	printf("%d\n", ans);
+	return 0;
+}
