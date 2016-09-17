122221df126fbbd2acfe71c21c72444e865d60b9
Zeyugao <gaozeyu@icloud.com>
2016-09-17 14:46:03 +0800

    Daily Commit && Fill the Instruction

---
+++ Sources/POJ2823 Sliding Window.cpp
@@ -0,0 +1,81 @@
+/*
+source:http://baike.baidu.com/view/3771451.htm#2_3
+这个是典型的固定k区间的单调队列。套用的本质思想是，如求最小值： 考虑这样的一个问题，在某个区间当中如果存在某两个元素A,B，满足A的下标小于B的下标，A的值大于B的值，那么A这个数就可以删掉，不再考虑。求最大值反之。
+具体的操作是：从加入第k个数开始，每插入做一次队列单调性更新：
+删队尾【单调性】，入队，删队首【下标范围k以内】，输出队首【即最值】。*/
+//当A>B时，但如果B比A更晚进来，必定会在A前面出去，这就说明，只要B在，A就不可能会是最小值，就可以放心地删除A
+
+/*
+Source:http://blog.csdn.net/kenden23/article/details/37598147
+
+1 如何入列，保存数据 -- 最小单调队列的时候， 所有数都入列一次，在新的数据准备入列的时候，增加判断，如果当前数值小于队尾数值，那么队尾数值就出列。空队列的时候直接入列。
+2 保存的数列是什么样的？
+举例吧： 1 3 -1 -3 5 3 6 7
+构建最小单调队列
+第一个数值1的时候，空队列，那么入列，得到： 1
+第二个数值3， 因为大于1；那么1不用出列，直接入列，得到： 1 3
+第三个数值-1，因为小于3，故此3出列，-1入列，继续判断-1小于1，故此1也出列，剩下队列得到： -1
+以此类推……
+根据实际情况，本题需要的是K大小端中的最小和最大值，那么就可以记录上各个数值的下标，加以判断。即下标不在需要的判断范围之内的就出来，不过这次是从队列头出列。因为去掉这些下标不合要求的数值之后，队列的头数值依然是最小值，故此是当前答案。*/
+
+#include<cstdio>
+const int MAX_N = 1000001;
+
+int N, K;
+int arr[MAX_N];
+int qu[MAX_N];
+int index[MAX_N];
+int ans[MAX_N];
+
+void getMins()
+{
+	int tail = 0, head = 1;	//初始化tail<head表示为空列
+	for (int i = 1; i < K; i++)	//初始化单调队列
+	{
+		while (tail >= head && qu[tail] >= arr[i]) tail--;
+		qu[++tail] = arr[i];		//记录可能的答案值
+		index[tail] = i;	//记录额外需要判断的信息
+	}
+	for (int i = K; i <= N; i++)
+	{
+		while (tail >= head && qu[tail] >= arr[i]) tail--; //不符合条件出列
+		qu[++tail] = arr[i];
+		index[tail] = i;
+		while (index[head] <= i-K) head++;
+		ans[i-K] = qu[head];	//ans从下标0开始记录
+	}
+}
+
+void getMaxs()
+{
+	int tail = 0, head = 1;
+	for (int i = 1; i < K; i++)	//初始化单调队列
+	{
+		while (tail >= head && qu[tail] <= arr[i]) tail--;
+		qu[++tail] = arr[i];
+		index[tail] = i;
+	}
+	for (int i = K; i <= N; i++)
+	{
+		while (tail >= head && qu[tail] <= arr[i]) tail--; //不符合条件出列
+		qu[++tail] = arr[i];
+		index[tail] = i;
+		while (index[head] <= i-K) head++;
+		ans[i-K] = qu[head];	//ans从下标0开始记录
+	}
+}
+
+int main()
+{
+	while (~scanf("%d %d", &N, &K))
+	{
+		for (int i = 1; i <= N; i++) scanf("%d", &arr[i]);
+		getMins();
+		for (int i = 0; i <= N-K; i++) printf("%d ", ans[i]);
+		putchar('\n');
+		getMaxs();
+		for (int i = 0; i <= N-K; i++) printf("%d ", ans[i]);
+		putchar('\n');
+	}
+	return 0;
+}
---
+++ Sources/Vijso P1193 扫雷.cpp
@@ -0,0 +1,68 @@
+#ifndef _DEBUG
+#pragma GCC optimize(2)
+#ifdef Full_Cpp_Compiler
+#include <codecvt>
+#endif
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <cstring>
+#include <fstream>
+#include <Windows.h>
+#include <iterator>
+#include <set>
+#include <process.h>
+#include <ctime>
+#include <utility>
+#include <cctype>
+#include <bitset>
+#include <stdarg.h>
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a>b?a:b)
+#endif // _DEBUG
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	long n, mine[10010] = { 0 }, dp[10010][2][2][2] = { 0 };
+	std::cin >> n;
+	for (int i = 1; i <= n; i++) {
+		std::cin >> mine[i];
+	}
+	//t-1,t,t+1
+	for (int i = 0; i <= 1; i++)
+		for (int j = 0; j <= 1; j++)
+			for (int k = 0; k <= 1; k++)
+				if ((i + j + k) == mine[1])
+					dp[1][i][j][k]++;
+	for (long t = 2; t <= n; t++)
+		for (int i/*t-1*/ = 0; i <= 1; i++)
+			for (int j/*t*/ = 0; j <= 1; j++)
+				for (int k/*t+1*/ = 0; k <= 1; k++)
+					for (int h/*t-2*/ = 0; h <= 1; h++)
+						if (i + j + k == mine[t] && h + i + j == mine[t - 1])
+							dp[t][i][j][k] += dp[t - 1][h][i][j];
+	/*
+	f(t, 2, n)
+		f(i, 0, 1)
+		f(j, 0, 1)
+		f(k, 0, 1)
+		f(h, 0, 1) {
+		if ((i + j + k) == mine[t] && (j + i + h) == mine[t - 1]) {
+			dp[t][i][j][k] += dp[t - 1][h][i][j];
+		}
+	}
+	*/
+	long res = 0;
+	for (int i = 0; i <= 1; i++)
+		for (int j = 0; j <= 1; j++)
+			for (int k = 0; k <=/*在这里的错误弄了好久。。。，最后一行后面不能再放地雷了*/ 0; k++)
+				res += dp[n][i][j][k];
+	std::cout << res << std::endl;
+	return 0;
+}
