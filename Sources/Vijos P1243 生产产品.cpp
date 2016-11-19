1c5ea388c4752ed723b390befd1bf33347b41f47
Elsa <gaozeyu@icloud.com>
2016-11-19 22:03:19 +0800

    Update Vijos P1243 生产产品.cpp

--- Sources/Vijos P1243 生产产品.cpp
+++ Sources/Vijos P1243 生产产品.cpp
@@ -1,4 +1,4 @@
-题意要求尽量缩短时间来完成一件产品，但是由于需要按照步骤，所以多线程的方式不必考虑。dp[i][j]=min（dp[k][p]）+（sum[i][j] - sum[k][j]），dp[i][j]表示完成前i个步骤，且最后一步是在第j台机器上完成。sum[k][j]表示第j台机器完成第1~k个步骤所需要的时间和，但是k与i的距离不宜超过规定的L。式子也可以这样写：dp[i][j]=min（dp[k][p] -  sum[k][j]）+ sum[i][j] ，那么就单单看这项min（dp[k][p] -  sum[k][j]）就行了，这不就是类似于在序列区间a[i-L]~a[i-1]之间找一个最小值的问题？那就可以用单调队列解决了。复杂度为O(n2*m)，是与L无关的。
+//题意要求尽量缩短时间来完成一件产品，但是由于需要按照步骤，所以多线程的方式不必考虑。dp[i][j]=min（dp[k][p]）+（sum[i][j] - sum[k][j]），dp[i][j]表示完成前i个步骤，且最后一步是在第j台机器上完成。sum[k][j]表示第j台机器完成第1~k个步骤所需要的时间和，但是k与i的距离不宜超过规定的L。式子也可以这样写：dp[i][j]=min（dp[k][p] -  sum[k][j]）+ sum[i][j] ，那么就单单看这项min（dp[k][p] -  sum[k][j]）就行了，这不就是类似于在序列区间a[i-L]~a[i-1]之间找一个最小值的问题？那就可以用单调队列解决了。复杂度为O(n2*m)，是与L无关的。
 
 //#include <bits/stdc++.h>
 #include <iostream>
@@ -96,7 +96,7 @@ int main()
     }
     return 0;
 }
-
+/×
 【思路】
 
 单调队列优化DP。
@@ -111,53 +111,53 @@ f[i][j]=min{f[k][p]-sum[k][j]}+sum[i][j]，i-L<=k<=i-1
 
 　　 表示f[i][j]时括号中可以取到的最小值，单调队列j维护区间[i-L,i-1]内a[][j]的最小值。
 
-注意单调队列写法。
-1 #include<cstdio>
- 2 #include<cstring>
- 3 #include<iostream>
- 4 using namespace std;
- 5 
- 6 const int maxn = 200000+10;
- 7 const int INF  = 1e9+1e9;
- 8 
- 9 int L[6],R[6],q[maxn][6];
-10 int f[maxn][6] , a[maxn][6] , b[maxn][6];
-11 int n,m,K,lim;
-12 
-13 void push(int i,int j) {
-14     while(L[j]<R[j] && a[i][j]<=a[q[R[j]-1][j]][j]) R[j]--;
-15     q[R[j]++][j]=i;
-16 }
-17 int pop(int i,int j) {
-18     while(L[j]<R[j] && q[L[j]][j]<i-lim) L[j]++;
-19     return a[q[L[j]][j]][j];
-20 }
-21 int read(int& x) {
-22     char c=getchar(); while(!isdigit(c))c=getchar();
-23     x=0; while(isdigit(c))x=x*10+c-'0',c=getchar();
-24 }
-25 int main() {
-26     read(n),read(m),read(K),read(lim);
-27     for(int i=1;i<=m;i++)
-28         for(int j=1;j<=n;j++) {
-29             read(b[j][i]);
-30             b[j][i]+=b[j-1][i];
-31         }
-32     for(int j=1;j<=m;j++)  push(0,j);
-33     for(int i=1;i<=n;i++)
-34     {
-35         for(int j=1;j<=m;j++)
-36             f[i][j]=pop(i,j)+b[i][j];
-37         for(int j=1;j<=m;j++) {            //向单调队列中添加a[i][] 
-38             a[i][j]=INF;
-39             for(int k=1;k<=m;k++) if(k!=j)
-40                 a[i][j]=min(a[i][j],f[i][k]);
-41             a[i][j]=a[i][j]-b[i][j]+K;
-42             push(i,j);
-43         }
-44     }
-45     int ans=f[1];
-46     for(int i=1;i<=m;i++) ans=min(ans,f[i]);
-47     printf("%d\n",ans);
-48     return 0;
-49 }
+注意单调队列写法。×/
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+using namespace std;
+ 
+const int maxn = 200000+10;
+const int INF  = 1e9+1e9;
+
+int L[6],R[6],q[maxn][6];
+int f[maxn][6] , a[maxn][6] , b[maxn][6];
+int n,m,K,lim;
+
+void push(int i,int j) {
+	while(L[j]<R[j] && a[i][j]<=a[q[R[j]-1][j]][j]) R[j]--;
+	q[R[j]++][j]=i;
+}
+int pop(int i,int j) {
+    while(L[j]<R[j] && q[L[j]][j]<i-lim) L[j]++;
+    return a[q[L[j]][j]][j];
+}
+int read(int& x) {
+    char c=getchar(); while(!isdigit(c))c=getchar();
+    x=0; while(isdigit(c))x=x*10+c-'0',c=getchar();
+}
+int main() {
+    read(n),read(m),read(K),read(lim);
+    for(int i=1;i<=m;i++)
+        for(int j=1;j<=n;j++) {
+            read(b[j][i]);
+            b[j][i]+=b[j-1][i];
+        }
+    for(int j=1;j<=m;j++)  push(0,j);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+            f[i][j]=pop(i,j)+b[i][j];
+        for(int j=1;j<=m;j++) {            //向单调队列中添加a[i][] 
+            a[i][j]=INF;
+            for(int k=1;k<=m;k++) if(k!=j)
+                a[i][j]=min(a[i][j],f[i][k]);
+            a[i][j]=a[i][j]-b[i][j]+K;
+            push(i,j);
+        }
+    }
+    int ans=f[1];
+    for(int i=1;i<=m;i++) ans=min(ans,f[i]);
+    printf("%d\n",ans);
+    return 0;
+}
